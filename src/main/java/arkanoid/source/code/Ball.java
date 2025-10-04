f0469739c41ddee5c4cc16ba0ab402d00f44b8ec
lynkdeptrai <linhvuthuy2901@gmail.com>
2025-10-04 19:25:33 +0700

    Thêm logic ball(Chưa xử lí khi false)

--- src/main/java/arkanoid/source/code/Ball.java
+++ src/main/java/arkanoid/source/code/Ball.java
@@ -16,11 +16,12 @@ public class Ball {
 
     private final double maxAngle = Math.toRadians(70);
 
-    private double dx=5;
-    private double dy=-5;
-    private double speed = Math.sqrt(dx*dx + dy*dy);
-    public boolean lost=false; //Thua
-    public boolean start=false;//Khi bấm space
+    private double dx = 5;
+    private double dy = -5;
+    private double speed = Math.sqrt(dx * dx + dy * dy);
+    public boolean lost = false; //Thua
+    public boolean start = false;//Khi bấm space
+
     public Ball(Paddle paddle) {
         this.x = paddle.getX() + paddle.getWIDTH() / 2;
         this.y = paddle.getY() - radius;
@@ -32,42 +33,37 @@ public class Ball {
     public void update(double SCREEN_WIDTH, double SCREEN_HEIGHT) {
         final double paddleCenterX = paddle.getX() + paddle.getWIDTH() / 2;
 
-        double tiLe = Math.abs((paddle.getWIDTH()/2)/(ball.getCenterX() - paddleCenterX) );
-        double goc = maxAngle * tiLe;
-        if(y>SCREEN_HEIGHT+radius){
-            lost=true;
+        double tiLe = Math.abs((ball.getCenterX() - paddleCenterX)/(paddle.getWIDTH() / 2) );
+        if (y > SCREEN_HEIGHT + radius) {
+            lost = true;
         }
-        if(lost==false) {
-            if(start==true)
-            {
-                x+=dx;
-                y+=dy;
+        if (lost == false) {
+            if (start == true) {
+                x += dx;
+                y += dy;
             }
-            if((x+radius)>=SCREEN_WIDTH){
-                x=SCREEN_WIDTH-radius;
-                dx=-dx;
+            if ((x + radius) >= SCREEN_WIDTH) {
+                x = SCREEN_WIDTH - radius;
+                dx = -dx;
             }
-            if((x-radius)<=0){
-                x=radius;
-                dx=-dx;
+            if ((x - radius) <= 0) {
+                x = radius;
+                dx = -dx;
             }
-            if((y-radius)<=0){
-                y=radius;
-                dy=-dy;
+            if ((y - radius) <= 0) {
+                y = radius;
+                dy = -dy;
             }
-            if(dy>0&&((y+radius >=paddle.getY())&&(y+radius <=paddle.getY()+paddle.getHEIGHT())))
-            {
-                if ((x> paddle.getX() && x < paddleCenterX)) {
-                    dx=Math.abs(speed*Math.cos(tiLe*maxAngle))*(-1);
-                    dy=Math.abs(speed*Math.sin(tiLe*maxAngle))*(-1);
-                }
-                else if ( x> paddleCenterX&&x < paddle.getX() + paddle.getWIDTH()) {
-                    dx=Math.abs(speed*Math.cos(tiLe*maxAngle));
-                    dy=Math.abs(speed*Math.sin(tiLe*maxAngle))*(-1);
-                }
-                else if(x==paddle.getX()+paddle.getWIDTH()/2){
-                    dx=5;
-                    dy=-5;
+            if (dy > 0 && ((y + radius >= paddle.getY()) && (y + radius <= paddle.getY() + paddle.getHEIGHT()))) {
+                if ((x > paddle.getX() && x < paddleCenterX)) {
+                    dx = Math.abs(speed * Math.sin(tiLe * maxAngle)) * (-1);
+                    dy = Math.abs(speed * Math.cos(tiLe * maxAngle)) * (-1);
+                } else if (x > paddleCenterX && x < paddle.getX() + paddle.getWIDTH()) {
+                    dx = Math.abs(speed * Math.sin(tiLe * maxAngle));
+                    dy = Math.abs(speed * Math.cos(tiLe * maxAngle)) * (-1);
+                } else if (x == paddle.getX() + paddle.getWIDTH() / 2) {
+                    dx = 5;
+                    dy = -5;
                 }
             }
         }
