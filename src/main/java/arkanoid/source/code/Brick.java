6b4f3a0fba814ba730b084a4334da8be5a977604
lynkdeptrai <linhvuthuy2901@gmail.com>
2025-10-11 09:14:46 +0700

    brick

--- src/main/java/arkanoid/source/code/Brick.java
+++ src/main/java/arkanoid/source/code/Brick.java
@@ -18,54 +18,69 @@ public class Brick {
     private double centerY;
     LinkedList<Rectangle> basic_bricks = new LinkedList<Rectangle>();
     LinkedList<blockBrick> block_brick = new LinkedList<blockBrick>();
-    public class blockBrick{
+
+    public class blockBrick {
         Rectangle brick;
         int times;
-        blockBrick(Rectangle brick, int times){
+
+        blockBrick(Rectangle brick, int times) {
             this.brick = brick;
             this.times = times;
         }
     }
+
     public Brick(double SCREEN_WIDTH) {
         this.x = 0;
         this.y = 60;
-        this.width =SCREEN_WIDTH/5;
+        this.width = SCREEN_WIDTH / 5;
         this.height = 30;
-        this.centerX=width/2;
-        this.centerY=height/2;
+        this.centerX = width / 2;
+        this.centerY = height / 2;
     }
-    public int [][]map1={{0,0,0,1,0},{1,2,2,1,0},{0,0,1,2,0}};
-    public int[][][] maps={map1};
-    public void khoiTao(Pane root){
-        for (int k=0;k<maps.length;k++){
+
+    public int[][] map1 = {
+            {0, 0, 1, 2, 0, 1, 0, 2, 0, 1},
+            {1, 2, 0, 0, 1, 2, 1, 0, 2, 0},
+            {0, 1, 2, 1, 0, 0, 2, 1, 0, 2},
+            {2, 0, 0, 1, 2, 1, 0, 2, 1, 0},
+            {1, 0, 2, 0, 1, 0, 2, 1, 0, 2},
+            {0, 2, 1, 0, 2, 1, 0, 0, 1, 2},
+            {2, 1, 0, 2, 1, 0, 2, 1, 0, 0},
+            {0, 0, 2, 1, 0, 2, 1, 0, 2, 1},
+            {1, 2, 0, 0, 1, 0, 2, 1, 0, 2},
+            {0, 1, 2, 1, 0, 2, 1, 0, 2, 0}
+    };
+    public int[][][] maps = {map1};
+
+    public void khoiTao(Pane root) {
+        for (int k = 0; k < maps.length; k++) {
             for (int i = 0; i < maps[k].length; i++) {
                 for (int j = 0; j < maps[k][i].length; j++) {
-                    Rectangle rec=new Rectangle();
+                    Rectangle rec = new Rectangle();
                     rec.setWidth(width);
                     rec.setHeight(height);
-                    rec.setX(x+j*width);
-                    rec.setY(y+i*height);
-                    if(maps[k][i][j]==1){
+                    rec.setX(x + j * width);
+                    rec.setY(y + i * height);
+                    if (maps[k][i][j] == 1) {
                         rec.setFill(Color.PINK);
                         rec.setStroke(Color.BLACK);
                         rec.setStrokeWidth(5);
                         root.getChildren().add(rec);
                         basic_bricks.add(rec);
-                    }
-                    else if(maps[k][i][j]==2){
+                    } else if (maps[k][i][j] == 2) {
                         rec.setFill(Color.RED);
                         rec.setStroke(Color.BLACK);
                         rec.setStrokeWidth(5);
                         root.getChildren().add(rec);
-                        block_brick.add(new blockBrick(rec,3));
-                    }
-                    else{
+                        block_brick.add(new blockBrick(rec, 3));
+                    } else {
                         continue;
                     }
                 }
             }
         }
     }
+
     public void vaCham(Ball ball, Pane root) {
         javafx.scene.shape.Circle circle = new javafx.scene.shape.Circle(ball.getX(), ball.getY(), ball.getRadius());
 
@@ -76,20 +91,14 @@ public class Brick {
             for (Rectangle brick : basic_bricks) {
                 Shape inter = Shape.intersect(ball.getBall(), brick);
                 if (inter.getBoundsInLocal().getWidth() > 0 && inter.getBoundsInLocal().getHeight() > 0) {
-                    if((ball.getY()>= brick.getY()&&ball.getY()<=brick.getY()+brick.getHeight())
-                            &&((ball.getX()+ball.getRadius()>=brick.getX()&&ball.getX()+ball.getRadius()<=brick.getX()+this.centerX)
-                            ||(ball.getX()-ball.getRadius()<=brick.getX()+brick.getWidth()&&ball.getX()-ball.getRadius()>=brick.getX()+this.centerX)))
-                    {
-                        ball.setVx(-ball.getVx());
-                        touchedBrick=brick;
-                    }
-                    if((ball.getX()>= brick.getX()&&ball.getX()<=brick.getX()+brick.getWidth())
-                            &&((ball.getY()+ball.getRadius()>=brick.getY()&&ball.getY()+ball.getRadius()<=brick.getY()+this.centerY)
-                            ||(ball.getY()-ball.getRadius()<=brick.getY()+brick.getHeight()&&ball.getY()-ball.getRadius()>=brick.getY()+this.centerY)))
-                    {
+                    if (ball.getX() >= brick.getX() && ball.getX() <= brick.getX() + brick.getWidth()) {
                         ball.setVy(-ball.getVy());
-                        touchedBrick=brick;
+                        touchedBrick = brick;
+                    } else if (ball.getY() >= brick.getY() && ball.getY() <= brick.getY() + brick.getHeight()) {
+                        ball.setVx(-ball.getVx());
+                        touchedBrick = brick;
                     }
+
                 }
             }
 
@@ -97,23 +106,15 @@ public class Brick {
                 for (blockBrick br : block_brick) {
                     Shape inter = Shape.intersect(circle, br.brick);
                     if (inter.getBoundsInLocal().getWidth() > 0 && inter.getBoundsInLocal().getHeight() > 0) {
-                        if((ball.getY()> br.brick.getY()&&ball.getY()<br.brick.getY()+br.brick.getHeight())
-                                &&((ball.getX()+ball.getRadius()>=br.brick.getX()&&ball.getX()+ball.getRadius()<=br.brick.getX()+this.centerX)
-                                ||(ball.getX()-ball.getRadius()<=br.brick.getX()+br.brick.getWidth()&&ball.getX()-ball.getRadius()>=br.brick.getX()+this.centerX)))
-                        {
-                            ball.setVx(-ball.getVx());
+                        if (ball.getX() >= br.brick.getX() && ball.getX() <= br.brick.getX() + br.brick.getWidth()) {
+                            ball.setVy(-ball.getVy());
                             br.times--;
-
                             if (br.times == 0) {
                                 touchedBlock = br;
                             }
                             break;
-                        }
-                        if((ball.getX()>= br.brick.getX()&&ball.getX()<=br.brick.getX()+br.brick.getWidth())
-                                &&((ball.getY()+ball.getRadius()>=br.brick.getY()&&ball.getY()+ball.getRadius()<=br.brick.getY()+this.centerY)
-                                ||(ball.getY()-ball.getRadius()<=br.brick.getY()+br.brick.getHeight()&&ball.getY()-ball.getRadius()>=br.brick.getY()+this.centerY)))
-                        {
-                            ball.setVy(-ball.getVy());
+                        } else if (ball.getY() >= br.brick.getY() && ball.getY() <= br.brick.getY() + br.brick.getHeight()) {
+                            ball.setVx(-ball.getVx());
                             br.times--;
                             if (br.times == 0) {
                                 touchedBlock = br;
