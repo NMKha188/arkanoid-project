e2a9f73d5a688d437cb511a398fb7a6818dc4895
phung1910 <hunghungb643@gmail.com>
2025-10-27 13:43:25 +0700

    them anh

--- src/main/java/arkanoid/source/code/gameplay/brick/HardBrick.java
+++ src/main/java/arkanoid/source/code/gameplay/brick/HardBrick.java
@@ -1,11 +1,12 @@
 package arkanoid.source.code.gameplay.brick;
 
+import arkanoid.source.code.gameplay.Texture;
 import javafx.scene.paint.Color;
 
 public class HardBrick extends Brick {
     public HardBrick(double x, double y) {
         super(x, y);
-        shape.setFill(Color.PURPLE);
+        Texture.applyTextureToBrick(shape, 6);
         hitPoints = 5;
     }
 }
--- src/main/java/arkanoid/source/code/gameplay/brick/NormalBrick.java
+++ src/main/java/arkanoid/source/code/gameplay/brick/NormalBrick.java
@@ -1,11 +1,12 @@
 package arkanoid.source.code.gameplay.brick;
 
+import arkanoid.source.code.gameplay.Texture;
 import javafx.scene.paint.Color;
 
 public class NormalBrick extends Brick {
     public NormalBrick(double x, double y) {
         super(x, y);
-        shape.setFill(Color.RED);
+        Texture.applyTextureToBrick(shape, 5);
         hitPoints = 3;
     }
 }
