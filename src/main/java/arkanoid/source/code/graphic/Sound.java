266d08d80950e7cd0bf0c277dbba04cfcec8bbdb
phung1910 <hunghungb643@gmail.com>
2025-11-11 22:44:48 +0700

    them sound 11/11

--- src/main/java/arkanoid/source/code/graphic/Sound.java
+++ src/main/java/arkanoid/source/code/graphic/Sound.java
@@ -1,24 +1,28 @@
 package arkanoid.source.code.graphic;
 
 import javafx.scene.media.AudioClip;
-import java.io.File;
+import java.net.URL;
 
 public class Sound {
-    private static final String BRICK_HIT_PATH = "C:/arkanoid-project/src/main/resources/arkanoid/resources/sounds/hit.mp3";
-    private static final String PADDLE_HIT_PATH = "C:/arkanoid-project/src/main/resources/arkanoid/resources/sounds/hit.mp3";
-    private static final String LOSE_LIFE_PATH = "C:/arkanoid-project/src/main/resources/arkanoid/resources/sounds/hit.mp3";
 
+    private static final String BRICK_HIT_PATH = "/arkanoid/resources/sounds/hit.mp3";
+    private static final String PADDLE_HIT_PATH = "/arkanoid/resources/sounds/hit.mp3";
+    private static final String LOSE_LIFE_PATH = "/arkanoid/resources/sounds/die.mp3";
+    private static final String THEME_SOUND = "/arkanoid/resources/sounds/theme_song.mp3";
+
+    private static final AudioClip themeSound = loadSound(THEME_SOUND);
     private static final AudioClip brickHitSound = loadSound(BRICK_HIT_PATH);
     private static final AudioClip paddleHitSound = loadSound(PADDLE_HIT_PATH);
     private static final AudioClip loseLifeSound = loadSound(LOSE_LIFE_PATH);
 
     private static AudioClip loadSound(String path) {
         try {
-            File file = new File(path);
-            if (file.exists()) {
-                return new AudioClip(file.toURI().toString());
+            URL resourceUrl = Sound.class.getResource(path);
+
+            if (resourceUrl != null) {
+                return new AudioClip(resourceUrl.toExternalForm());
             } else {
-                System.err.println("Không tìm thấy tệp âm thanh: " + path);
+                System.err.println("Không tìm thấy tệp âm thanh trong classpath: " + path);
                 return null;
             }
         } catch (Exception e) {
@@ -27,6 +31,13 @@ public class Sound {
             return null;
         }
     }
+
+    public static void playThemeSound() {
+        if(themeSound != null) {
+            themeSound.play();
+        }
+    }
+
     public static void playBrickHit() {
         if (brickHitSound != null) {
             brickHitSound.play();
