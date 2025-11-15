127aa774e91cf72d6c1b9cbb9ba6039a45dafd03
phung1910 <hunghungb643@gmail.com>
2025-11-15 23:40:51 +0700

    them anh 15/11

--- src/main/java/arkanoid/source/code/graphic/Sound.java
+++ src/main/java/arkanoid/source/code/graphic/Sound.java
@@ -10,13 +10,18 @@ public class Sound {
     private static final String THEME_SOUND = "/arkanoid/resources/sounds/theme.mp3";
     private static final String EXPLOSION_PATH = "/arkanoid/resources/sounds/explode.mp3";
     private static final String BRICK_HIT = "/arkanoid/resources/sounds/brick_hit.mp3";
-    private static final String THUNDER_PATH = "/arkanoid/resources/sounds/thunder.mp3";
+    private static final String THUNDER_PATH = "/arkanoid/resources/sounds/thunder_sound.mp3";
+    private static final String POWERUP = "/arkanoid/resources/sounds/powerup.mp3";
+    private static final String WIN = "/arkanoid/resources/sounds/win.mp3";
+
     private static final AudioClip themeSound = loadSound(THEME_SOUND);
     private static final AudioClip HitSound = loadSound(HIT_PATH);
     private static final AudioClip loseLifeSound = loadSound(LOSE_LIFE_PATH);
     private static final AudioClip explosionSound = loadSound(EXPLOSION_PATH);
     private static final AudioClip brickhitSound = loadSound(BRICK_HIT);
     private static final AudioClip thunderSound = loadSound(THUNDER_PATH);
+    private static final AudioClip powerupSound = loadSound(POWERUP);
+    private static final AudioClip winSound = loadSound(WIN);
 
     private static AudioClip loadSound(String path) {
         try {
@@ -64,6 +69,16 @@ public class Sound {
     public static void playThunder() {
         if (thunderSound != null) {
             thunderSound.play();
+        }
+    }
+    public static void playPowerup() {
+        if(powerupSound != null) {
+            powerupSound.play();
+        }
+    }
+    public static void playWin() {
+        if(winSound != null) {
+            winSound.play();    
         }
     }
 }
